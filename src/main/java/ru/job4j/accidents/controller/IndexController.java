8266770fd5d65e7ba76421b04bd8582a8a5d51e5
vpesterev <v.pesterev@m-developer.ru>
2023-08-02 18:00:39 +0300

    Решение задач уровня Middle. Категория : 3.5. Spring boot Тема : 3.4.2. MVC 1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,12 +1,13 @@
 package ru.job4j.accidents.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.ui.Model;
 @Controller
 public class IndexController {
 
     @GetMapping("/index")
-    public String getIndex() {
+    public String getIndex(Model model) {
+        model.addAttribute("user", "Petr Arsentev");
         return "index";
     }
 
