54b7e3843cdfb957f67bb370fe1d2c006bcb678d
ViacheslavPesterev <pesterevvv@gmail.com>
2023-11-07 19:17:11 +0300

    Решение задач уровня Middle. Категория : 3.5. Spring boot Тема : 3.5.5. Tests 0. Spring test [#6881] (правка замечания CI  - 3)

--- src/test/java/ru/job4j/accidents/AccidentControllerTest.java
+++ src/test/java/ru/job4j/accidents/AccidentControllerTest.java
@@ -3,17 +3,18 @@ package ru.job4j.accidents;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class AccidentControllerTest {
 
     @Autowired
--- src/test/java/ru/job4j/accidents/IndexControllerTest.java
+++ src/test/java/ru/job4j/accidents/IndexControllerTest.java
@@ -3,16 +3,17 @@ package ru.job4j.accidents;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class IndexControllerTest {
 
     @Autowired
